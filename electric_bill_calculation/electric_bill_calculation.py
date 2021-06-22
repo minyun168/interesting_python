1e55cddd4af00619b64131bc4712ec57cd173238
minyun168 <minyun168@outlook.com>
2021-06-22 17:48:22 +0900

    Create electric_bill_calculation.py

---
+++ electric_bill_calculation/electric_bill_calculation.py
@@ -0,0 +1,6 @@
+﻿import datetime
+dt_now = datetime.datetime.now()
+print('現在の時刻は%s' %dt_now)
+dt_before = datetime.datetime(year = 2021, month = 5, day = 29)
+print('前の時刻は%s' %dt_before)
+delta = dt_now - dt_before
