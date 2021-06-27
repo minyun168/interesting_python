2c1e92c4f8a35943aac980bc197e9e71a2b6196a
minyun168 <minyun168@outlook.com>
2021-06-28 00:06:06 +0900

    Update electric_bill_calculation.py

--- electric_bill_calculation/electric_bill_calculation.py
+++ electric_bill_calculation/electric_bill_calculation.py
@@ -1,6 +1,14 @@
-﻿import datetime
+﻿#! python2.7
+# The program will caculate the electrict bill of everyone who live in the sharehouse whenever he started live in.
+
+
+import datetime
 dt_now = datetime.datetime.now()
 print('現在の時刻は%s' %dt_now)
-dt_before = datetime.datetime(year = 2021, month = 5, day = 29)
-print('前の時刻は%s' %dt_before)
-delta = dt_now - dt_before
+dt_before1 = datetime.datetime(year = 2021, month = 5, day = 29)
+dt_before2 = datetime.datetime(year = 2021, month = 5, day = 20)
+print('前の時刻は%s%s'   %(dt_before1,dt_before2))
+delta1 = dt_now - dt_before1
+delta2 = dt_now - dt_before2
+print('時間差は%s' %delta1)
+
